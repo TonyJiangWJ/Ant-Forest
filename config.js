6aadfe567ea5c79ed6ff6798dd921c501933b177
NickHopps <476335433@qq.com>
2019-01-12 12:44:46 +0800

    巨量更新

---
+++ config.js
@@ -0,0 +1,17 @@
+/*
+* @Author: NickHopps
+* @Last Modified by:   NickHopps
+* @Last Modified time: 2019-01-10 02:24:41
+* @Description: 配置文件
+*/
+
+// 执行配置
+var config = {
+  discern: {prime: "#31ab7c", extra: [[28, 42, "#ffffff"], [87, 87, "#23a372"]]},
+  password: "52897",
+  max_wait_time: 20,
+  max_retry_times: 3,
+  max_repeat_times: 10
+};
+
+module.exports = config;
