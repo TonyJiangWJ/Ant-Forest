26d9ff59edd182542c72f773bd3b0041aafb6f57
TonyJiangWJ <tonyjiangwj@gmail.com>
2020-05-01 17:30:18 +0800

    update outdate files

--- resources/for_update/OutdateFiles.js
+++ resources/for_update/OutdateFiles.js
@@ -3,7 +3,7 @@
  * @Date: 2020-05-01 12:28:02
  * @Last Modified by: TonyJiangWJ
 <<<<<<< HEAD
- * @Last Modified time: 2020-05-01 17:10:39
+ * @Last Modified time: 2020-05-01 17:30:10
 =======
  * @Last Modified time: 2020-05-01 17:09:22
 >>>>>>> master
@@ -29,9 +29,7 @@ module.exports = [
   'test/ReachBottomTest.js',
   'test/TestLockStorage.js',
   'test/queueTest',
-  'test/全局悬浮窗显示-音量上键关闭.js',
   'test/功能测试-百度识图.js',
-  'test/功能测试-百度识图缓存信息.js',
   'test/图片截取.js',
   'test/校验保护罩.js',
   'test/测试files.exists.ign.js',
@@ -41,6 +39,5 @@ module.exports = [
   'unit/统计能量增长信息.js',
   'unit/能量收集统计.js',
   'unit/功能测试-任务队列测试.js',
-  'unit/展示今日收集能量信息.js',
   'unit/获取当前页面的布局信息-请放置在unit文件夹下.js'
 ]
