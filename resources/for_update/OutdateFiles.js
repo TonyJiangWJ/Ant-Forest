584f92687942c46630ebeba0d178685b51ab8e84
TonyJiangWJ <tonyjiangwj@gmail.com>
2020-05-01 12:49:13 +0800

    添加需要移除的文件列表

--- resources/for_update/OutdateFiles.js
+++ resources/for_update/OutdateFiles.js
@@ -2,7 +2,7 @@
  * @Author: TonyJiangWJ
  * @Date: 2020-05-01 12:28:02
  * @Last Modified by: TonyJiangWJ
- * @Last Modified time: 2020-05-01 12:32:27
+ * @Last Modified time: 2020-05-01 12:48:46
  * @Description: 更新后需要移除的文件
  */
 module.exports = [
@@ -17,4 +17,26 @@ module.exports = [
   'lib/TryRequestScreenCapture.js',
   'lib/WidgetUtils.js',
   'extends/MultiTouchCollect-demo.js',
+  'test/AES_Test.js',
+  'test/BitCheckTest.js',
+  'test/ColorDetectTest.js',
+  'test/FriendListGet.js',
+  'test/GestureLockTest.js',
+  'test/ReachBottomTest.js',
+  'test/TestLockStorage.js',
+  'test/queueTest',
+  'test/全局悬浮窗显示-音量上键关闭.js',
+  'test/功能测试-百度识图.js',
+  'test/功能测试-百度识图缓存信息.js',
+  'test/图片截取.js',
+  'test/校验保护罩.js',
+  'test/测试files.exists.ign.js',
+  'test/测试files.exists.js',
+  'test/测试查看点击区域.js',
+  'test/能量球检测.js',
+  'unit/统计能量增长信息.js',
+  'unit/能量收集统计.js',
+  'unit/功能测试-任务队列测试.js',
+  'unit/展示今日收集能量信息.js',
+  'unit/获取当前页面的布局信息-请放置在unit文件夹下.js',
 ]
