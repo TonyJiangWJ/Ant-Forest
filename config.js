631f344ecb94249d513aa0b95aa5875a3afa458b
TonyJiangWJ <tonyjiangwj@gmail.com>
2020-05-01 12:39:57 +0800

    修改部分配置,更新时清理过期文件

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * @Author: TonyJiangWJ
  * @Date: 2019-12-09 20:42:08
  * @Last Modified by: TonyJiangWJ
- * @Last Modified time: 2020-05-01 11:24:19
+ * @Last Modified time: 2020-05-01 12:08:13
  * @Description: 
  */
 'ui';
@@ -35,6 +35,8 @@ let default_config = {
   show_debug_log: true,
   show_engine_id: false,
   develop_mode: false,
+  // 开发用开关，截图并保存一些图片
+  cutAndSaveCountdown: false,
   auto_lock: false,
   lock_x: 150,
   lock_y: 970,
@@ -58,12 +60,10 @@ let default_config = {
   finger_img_pixels: 2300,
   thread_pool_size: 4,
   thread_pool_max_size: 8,
-  thread_pool_queue_size: 256,
+  thread_pool_queue_size: 16,
   thread_pool_waiting_time: 5,
   white_list: [],
 
-  // 只在AutoJS中能打开，定时不能打开时 尝试开启这个 设为true
-  fuck_miui11: false,
   // 单脚本模式 是否只运行一个脚本 不会同时使用其他的 开启单脚本模式 会取消任务队列的功能。
   // 比如同时使用蚂蚁庄园 则保持默认 false 否则设置为true 无视其他运行中的脚本
   single_script: false,
@@ -304,6 +304,8 @@ if (!isRunningMode) {
     ui.saveLogFileChkBox.setChecked(config.saveLogFile)
     ui.showEngineIdChkBox.setChecked(config.show_engine_id)
     ui.developModeChkBox.setChecked(config.develop_mode)
+    ui.cutAndSaveCountdownChkBox.setChecked(config.cutAndSaveCountdown)
+    ui.developModeContainer.setVisibility(config.develop_mode ? View.VISIBLE : View.GONE)
     ui.fileSizeInpt.text(config.back_size + '')
     ui.fileSizeContainer.setVisibility(config.saveLogFile ? View.VISIBLE : View.INVISIBLE)
 
@@ -550,10 +552,15 @@ if (!isRunningMode) {
                     <text text="延迟启动时间（秒）:" />
                     <input layout_weight="70" inputType="number" id="delayStartTimeInpt" layout_weight="70" />
                   </horizontal>
+                  <checkbox id="developModeChkBox" text="是否启用开发模式" />
+                  <vertical id="developModeContainer" gravity="center">
+                    <text text="脚本执行时保存图片，未启用开发模式时依旧有效:" margin="5 0" textSize="14sp"/>
+                    <checkbox id="cutAndSaveCountdownChkBox" text="是否保存开发用的图片" />
+                  </vertical>
+                  <horizontal w="*" h="1sp" bg="#cccccc" margin="5 0"></horizontal>
                   {/* 是否显示debug日志 */}
                   <checkbox id="showDebugLogChkBox" text="是否显示debug日志" />
                   <checkbox id="showEngineIdChkBox" text="是否在控制台中显示脚本引擎id" />
-                  <checkbox id="developModeChkBox" text="是否启用开发模式" />
                   <horizontal gravity="center">
                     <checkbox id="saveLogFileChkBox" text="是否保存日志到文件" />
                     <horizontal padding="10 0" id="fileSizeContainer" gravity="center" layout_weight="75">
@@ -641,7 +648,8 @@ if (!isRunningMode) {
                         <input layout_weight="70" inputType="number" id="friendListScrollTimeInpt" layout_weight="70" />
                       </horizontal>
                     </vertical>
-                    <text text="可收取小手指的绿色像素点个数，1080P时小于2300判定为可收取，其他分辨率需要自行修改=2300*缩小比例^2" textSize="10sp" />
+                    <text text="可收取小手指的绿色像素点个数，1080P时小于2300判定为可收取，其他分辨率需要自行修改=2300*缩放比例^2" textSize="10sp" />
+                    <text text="如果还是不行，请分析日志进行调整，日志中会打印同色点个数" textSize="10sp" />
                     <horizontal gravity="center" >
                       <text text="小手指像素点个数:" />
                       <input layout_weight="70" inputType="number" id="fingerImgPixelsInpt" layout_weight="70" />
@@ -666,6 +674,7 @@ if (!isRunningMode) {
                   <vertical id="useOcrParentContainer">
                     <checkbox id="useOcrChkBox" text="是否启用百度的OCR识别倒计时" />
                     <vertical id="useOcrContainer">
+                      <text text="缓存数据仅仅是像素点个数和倒计时的键值对，所以可能返回的是错误的值" textSize="10sp" />
                       <checkbox id="ocrUseCacheChkBox" text="是否从缓存中获取OCR识别的倒计时，非精确值" />
                       <checkbox id="saveBase64ImgInfoChkBox" text="是否记录图片Base64数据到日志" />
                       <text id="ocrInvokeCount" textSize="12sp" />
@@ -1303,6 +1312,11 @@ if (!isRunningMode) {
 
     ui.developModeChkBox.on('click', () => {
       config.develop_mode = ui.developModeChkBox.isChecked()
+      ui.developModeContainer.setVisibility(config.develop_mode ? View.VISIBLE : View.GONE)
+    })
+
+    ui.cutAndSaveCountdownChkBox.on('click', () => {
+      config.cutAndSaveCountdown = ui.cutAndSaveCountdownChkBox.isChecked()
     })
 
     ui.saveLogFileChkBox.on('click', () => {
