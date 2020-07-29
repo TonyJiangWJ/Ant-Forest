7edb1df593b3d880ebed607511ea2c83ad0d2117
TonyJiangWJ <tonyjiangwj@gmail.com>
2020-07-29 23:35:29 +0800

    ???

--- core/Ant_forest.js
+++ core/Ant_forest.js
@@ -1,7 +1,7 @@
 /*
  * @Author: NickHopps
  * @Last Modified by: TonyJiangWJ
- * @Last Modified time: 2020-07-29 17:08:58
+ * @Last Modified time: 2020-07-29 23:35:17
  * @Description: 蚂蚁森林操作集
  */
 let { config: _config } = require('../config.js')(runtime, this)
@@ -371,7 +371,7 @@ function Ant_forest () {
       if (_config.never_stop) {
         let reactiveTime = _config.getReactiveTime() || 60
         if (_commonFunctions.isEmpty(_min_countdown) || _min_countdown > reactiveTime) {
-          debugInfo(['永不停止模式，已经获取倒计时[{}]大于重新激活时间，设置倒计时时间为：{}', _min_countdowm, reactiveTime])
+          debugInfo(['永不停止模式，已经获取倒计时[{}]大于重新激活时间，设置倒计时时间为：{}', _min_countdown, reactiveTime])
           _min_countdown = reactiveTime
         }
         _has_next = true
