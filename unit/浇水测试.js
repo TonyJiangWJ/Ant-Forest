ee43d78ec9add8b40200b49b7aa19fd17b52b53b
TonyJiangWJ <tonyjiangwj@gmail.com>
2020-08-25 18:44:08 +0800

    浇水功能修复

--- unit/浇水测试.js
+++ unit/浇水测试.js
@@ -2,20 +2,16 @@
  * @Author: TonyJiangWJ
  * @Date: 2019-12-19 10:53:08
  * @Last Modified by: TonyJiangWJ
- * @Last Modified time: 2020-08-25 18:37:48
+ * @Last Modified time: 2020-08-25 18:38:24
  * @Description: 
  */
 requestScreenCapture()
 let { config } = require('../config.js')(runtime, this)
 let singletonRequire = require('../lib/SingletonRequirer.js')(runtime, this)
 let WidgetUtils = singletonRequire('WidgetUtils')
-let count = 0
 let configedAmount = config.targetWateringAmount
 config.targetWateringAmount = 66
-while (count++ < 3) {
-  WidgetUtils.wateringFriends()
-  sleep(1500)
-}
+
+WidgetUtils.wateringFriends()
 config.targetWateringAmount = configedAmount
 toast('done')
-back()
