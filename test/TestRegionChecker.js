fe58423095348abe2c69e7085a84ac22276a737d
TonyJiangWJ <tonyjiangwj@gmail.com>
2020-10-22 23:03:08 +0800

    update to 1.3.3.2

--- test/TestRegionChecker.js
+++
@@ -1,6 +0,0 @@
-let sRequire = require('../lib/SingletonRequirer.js')(runtime, this)
-let commonFunction = sRequire('CommonFunction')
-
-commonFunction.ensureRegionInScreen([100, 1000, 1000, 1500], 50, 50)
-
-toastLog('done')
