f001cf20779a9b10fa9a4b5f322b1ab571e99bec
TonyJiangWJ <tonyjiangwj@gmail.com>
2021-11-26 00:24:15 +0800

    删除基本用不到的测试文件

--- test/测试查看tesserac_ocr缓存.js
+++
@@ -1,9 +0,0 @@
-
-let { config: _config, storage_name: _storage_name } = require('../config.js')(runtime, this)
-const pointNumberStorage = storages.create(_storage_name + '_tesserac_point_num')
-let md5Map = pointNumberStorage.get('md5_number_map')
-console.log('md5 map: ' + JSON.stringify(md5Map))
-let clear = false
-if (clear) {
-  pointNumberStorage.put('md5_number_map', {})
-}
