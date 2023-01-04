835e9277c1e8159eb5eee4d6a5e370f99a8d6a31
TonyJiangWJ <tonyjiangwj@gmail.com>
2023-01-04 21:19:20 +0800

    增加版本统计功能

--- test/测试能量值获取.js
+++ test/测试能量值获取.js
@@ -2,7 +2,7 @@
  * @Author: TonyJiangWJ
  * @Date: 2020-04-30 17:17:27
  * @Last Modified by: TonyJiangWJ
- * @Last Modified time: 2022-11-27 21:39:24
+ * @Last Modified time: 2022-12-26 22:47:51
  * @Description: 
  */
 let { config } = require('../config.js')(runtime, global)
@@ -16,12 +16,12 @@ config.enable_visual_helper = true
 let visualHelper = new _VisualHelper()
 visualHelper.init()
 visualHelper.disableTip()
-let youGet = widgetUtils.widgetGetOne('你收取TA')
+let youGet = widgetUtils.widgetGetOne('你收取TA.*')
 if (youGet) {
   let bd = youGet.bounds()
-  let region = [bd.left - 5, bd.bottom - 5, bd.right - bd.left + 10, (bd.bottom - bd.top) * 1.5]
+  let region = [bd.left - 5, bd.top - 5, bd.right - bd.left + 10, (bd.bottom - bd.top) * 2.2]
   visualHelper.addRectangle('查找收取能量范围', region)
-  let energySum = widgetUtils.widgetGetOne(/\d+g/, null, null, false, matcher => matcher.boundsInside(region[0], region[1], region[0] + region[2], region[1] + region[3]))
+  let energySum = widgetUtils.widgetGetOne(/.*\d+g/, null, null, false, matcher => matcher.boundsInside(region[0], region[1], region[0] + region[2], region[1] + region[3]))
   if (energySum) {
     if (energySum.desc()) {
       toastLog('能量值：' + energySum.desc().match(/\d+/))
