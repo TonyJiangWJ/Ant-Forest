f1947591fbac4a53ff763393a7ee735b12c6c1f6
TonyJiangWJ <tonyjiangwj@gmail.com>
2024-03-27 23:15:36 +0800

    修复打印启动参数时可能导致stackoverflow的问题

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: NickHopps
  * @Last Modified by: TonyJiangWJ
- * @Last Modified time: 2024-03-25 11:28:14
+ * @Last Modified time: 2024-03-27 23:14:44
  * @Description: 蚂蚁森林自动收能量
  */
 console.warn('如遇语法报错，请从README下载最新版的AutoJS，旧版本不维护，不适配')
@@ -126,7 +126,10 @@ try {
 
 logInfo('解锁成功')
 let executeArguments = engines.myEngine().execArgv
-debugInfo(['启动参数：{}', JSON.stringify(executeArguments)])
+// 部分设备中参数有脏东西 可能导致JSON序列化异常
+if (Object.keys(executeArguments).length < 5 && executeArguments && executeArguments.intent && Object.keys(executeArguments.intent) < 5) {
+  debugInfo(['启动参数：{}', JSON.stringify(executeArguments)])
+}
 // 定时启动的任务, 将截图权限滞后请求
 if (!executeArguments.intent || executeArguments.executeByDispatcher) {
   commonFunctions.requestScreenCaptureOrRestart()
